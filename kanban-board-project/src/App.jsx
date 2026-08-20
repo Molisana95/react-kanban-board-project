d056aac9dd36af39b9b99c2487efd3aa024361b8
Molisana95 <loconte.jacopo@gmail.com>
2026-08-20 17:25:12 +0200

    Implemented the mandatory authentication so that each one of the users will have its own list of tasks.

--- kanban-board-project/src/App.jsx
+++ kanban-board-project/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import TaskForm from "./components/TaskForm";
 import Columns from "./components/Columns";
 import "./style.css";
-import { collection, addDoc, serverTimestamp, query, orderBy, onSnapshot, doc, updateDoc, deleteDoc } from "firebase/firestore";
-import { db } from "./firebase";
+import { collection, addDoc, serverTimestamp, query, orderBy, onSnapshot, doc, updateDoc, deleteDoc, where } from "firebase/firestore";
+import { db, auth, googleProvider } from "./firebase";
+import { getAuth, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth"
 
 export const AppContext = React.createContext();
 
 export default function App() {
     const [tasks, setTasks] = React.useState([]);
+    const [currentUser, setCurrentUser] = React.useState(null)
+    const [loading, setLoading] = React.useState(true)
 
     // funzione per aggiungere un task al database Firestore
     const handleAddTask = async (text) => {
@@ -16,7 +19,8 @@ export default function App() {
             await addDoc(collection(db, "tasks"), {
                 text: text,
                 status: "to-do",
-                createdAt: serverTimestamp()
+                createdAt: serverTimestamp(),
+                userId: currentUser.uid
             })
         } catch (error) {
             console.error("Error adding document: ", error);
@@ -46,8 +50,20 @@ export default function App() {
     }
 
     React.useEffect(() => {
+        //listener per capire quando un utente entra o esce
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        //cleanup function
+        return () => unsubscribe();
+    }, [])
+
+    React.useEffect(() => {
         // creazione query della collezione in Firestore per ottenere i task ordinati ("createdAt", "desc")
-        const q = query(collection(db, "tasks"), orderBy("createdAt", "desc"));
+        if(!currentUser) return
+        // alla fine controllare con F12 se Firestore dà un errore azzurro; seguire istruzioni
+        const q = query(collection(db, "tasks"), where("userId", "==", currentUser.uid), orderBy("createdAt", "desc"));
         const unsubscribe = onSnapshot(q, (snapshot) => {
             const tasksData = snapshot.docs.map(doc => ({
                 id: doc.id,
@@ -58,14 +74,58 @@ export default function App() {
         });
         // cleanup function
         return () => unsubscribe();
-    }, []);
+    }, [currentUser]);
+
+
+    //funzioni login & logout
+
+    const handleLogin = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            console.error("Error during the login with Google:", error)
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.error("Error during the logout:", error)
+        }
+    }
 
     console.log(tasks);
+
+    if(loading) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+
+    if(!currentUser) {
+        return (
+            <div className="login-container">
+                <h1>Kanban Board</h1>
+                <p>Login do set up your personal tasks.</p>
+                <button className="login-btn" onClick={handleLogin}>
+                    <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google" />
+                    Accedi con Google
+                </button>
+            </div>
+        )
+        
+    }
 
     return (
         <AppContext.Provider value={{ tasks, handleAddTask, handleUpdateTaskStatus, handleDeleteTask }}>
             <div className="app-container">
                 <div className="header-container">
+                    <div className="user-profile">
+                        <img src={currentUser.photoURL} alt={currentUser.displayName} />
+                        <h3 className="user-name">{currentUser.displayName}</h3>
+                        <button className="logout-btn" onClick={handleLogout}>Logout</button>
+                    </div>
                     <h1>Kanban Board</h1>
                     <TaskForm onAddTask={handleAddTask} />
                 </div>
