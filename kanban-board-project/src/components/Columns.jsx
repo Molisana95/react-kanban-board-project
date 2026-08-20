1eb41179a772e6e632d660a9ef842b3c42607975
Molisana95 <loconte.jacopo@gmail.com>
2026-08-20 15:44:36 +0200

    Added a number that represents the number of tasks in each list

--- kanban-board-project/src/components/Columns.jsx
+++ kanban-board-project/src/components/Columns.jsx
@@ -4,11 +4,14 @@ import { AppContext } from "../App";
 
 export default function Columns({ title, statusClass }) {
     const { tasks } = React.useContext(AppContext);
-    
+    const filteredTasks = tasks?.filter(task => task.status === statusClass)
     return (
-        <div className={`list ${statusClass}`}>
-            <h5>{title}</h5>
-            {tasks?.filter(task => task.status === statusClass).map(task => (
+        <div className={`list ${statusClass}`}> 
+            <div className="list-header">
+                <h5>{title}</h5>
+                <h6>{filteredTasks.length}</h6>
+            </div>
+            {filteredTasks.map(task => (
                 <TaskCard 
                     key={task.id} 
                     task={task} 
